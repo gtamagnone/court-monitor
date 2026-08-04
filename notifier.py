d5ee05549834f091a336d9fd8352edd243c0473f
Gabriel <66264932+gtamagnone@users.noreply.github.com>
2026-08-04 14:40:06 -0300

    add Telegram notifier

---
+++ notifier.py
@@ -0,0 +1,14 @@
+
+def send_telegram_message(token, chat_id, message):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+    response = requests.post(
+        url,
+        data={
+            "chat_id": chat_id,
+            "text": message,
+        },
+        timeout=15,
+    )
+
+    response.raise_for_status()
